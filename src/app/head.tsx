35b710a3eae3588885e5bb80608452817f61fe17
Huy Nguyen <huyn.nguyen95@gmail.com>
2023-02-02 09:57:15 -0600

    initial commit

---
+++ src/app/head.tsx
@@ -0,0 +1,13 @@
+export default function Head() {
+  return (
+    <>
+      <title>Reddit Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta
+        name="description"
+        content="Rebuilding Reddit website with Next.js, Tailwind, and Firebase"
+      />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
